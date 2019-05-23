735d5bee8b67183baa940b11035e49fda4342267
Caio Siqueira <caiosiqueira@outlook.com>
2019-05-23 11:27:13 -0300

    Primeiro Commit

---
+++ frontend/src/common/template/content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Grid from '../layout/grid';
+
+export default props => (
+    <div>
+    <section className="content">{props.children}</section>
+    </div>
+)
+
+
+;
